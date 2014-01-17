cfee1da24e1df88060e0069c53a28702f2348a36
jhorman <jhorman@gmail.com>
2014-01-16 16:57:49 -0800

    Adding support for logging related variables

--- bin/gen-pgbouncer-conf.sh
+++ bin/gen-pgbouncer-conf.sh
@@ -32,6 +32,9 @@ max_client_conn = 100
 default_pool_size = ${PGBOUNCER_DEFAULT_POOL_SIZE:-1}
 reserve_pool_size = ${PGBOUNCER_RESERVE_POOL_SIZE:-1}
 reserve_pool_timeout = ${PGBOUNCER_RESERVE_POOL_TIMEOUT:-5.0}
+log_connections = ${PGBOUNCER_LOG_CONNECTIONS:-1}
+log_disconnections = ${PGBOUNCER_LOG_DISCONNECTIONS:-1}
+log_pooler_errors = ${PGBOUNCER_LOG_POOLER_ERRORS:-1}
 [databases]
 EOFEOF
 
