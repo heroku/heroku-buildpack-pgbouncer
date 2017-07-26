heroku/heroku-buildpack-pgbouncer

commit 948fa643ef8fde0a6ab134f2c697167de264ded3

Fixed continuous config appending (#83)

bin/gen-pgbouncer-conf.sh
@@ -19,7 +19,7 @@ else
 fi
 
 mkdir -p /app/vendor/stunnel/var/run/stunnel/
-cat >> /app/vendor/stunnel/stunnel-pgbouncer.conf << EOFEOF
+cat > /app/vendor/stunnel/stunnel-pgbouncer.conf << EOFEOF
 foreground = yes
 
 options = NO_SSLv2
@@ -32,7 +32,7 @@ ciphers = HIGH:!ADH:!AECDH:!LOW:!EXP:!MD5:!3DES:!SRP:!PSK:@STRENGTH
 debug = ${PGBOUNCER_STUNNEL_LOGLEVEL:-notice}
 EOFEOF
 
-cat >> /app/vendor/pgbouncer/pgbouncer.ini << EOFEOF
+cat > /app/vendor/pgbouncer/pgbouncer.ini << EOFEOF
 [pgbouncer]
 listen_addr = 127.0.0.1
 listen_port = 6000
@@ -86,7 +86,7 @@ connect = $DB_HOST:$DB_PORT
 retry = ${PGBOUNCER_CONNECTION_RETRY:-"no"}
 EOFEOF
 
-  cat >> /app/vendor/pgbouncer/users.txt << EOFEOF
+  cat > /app/vendor/pgbouncer/users.txt << EOFEOF
 "$DB_USER" "$DB_MD5_PASS"
 EOFEOF
 

commit e415af3f9c1f75f2968ee1246cec21d99e932fd8

Fix appending users.txt which broke in #83
The users.txt file must be appended to as multiple postgres dbs in
POSTGRES_URLS, we need to add all the creds to the users.txt file, not
reset it for every entry in POSTGRES_URLS.

bin/gen-pgbouncer-conf.sh
@@ -86,7 +86,7 @@ connect = $DB_HOST:$DB_PORT
 retry = ${PGBOUNCER_CONNECTION_RETRY:-"no"}
 EOFEOF
 
-  cat > /app/vendor/pgbouncer/users.txt << EOFEOF
+  cat >> /app/vendor/pgbouncer/users.txt << EOFEOF
 "$DB_USER" "$DB_MD5_PASS"
 EOFEOF
 