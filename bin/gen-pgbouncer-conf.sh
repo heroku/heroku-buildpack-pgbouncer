d0f926d1406361970365036b1888559c38bfa534
jhwillett <jhw@prosperworks.com>
2018-09-05 14:51:29 -0700

    dedupe low-level section

--- bin/gen-pgbouncer-conf.sh
+++ bin/gen-pgbouncer-conf.sh
@@ -66,30 +66,6 @@ tcp_keepidle = ${PGBOUNCER_TCP_KEEPIDLE:-7200}
 tcp_keepintvl = ${PGBOUNCER_TCP_KEEPINTVL:-75}
 EOFEOF
 
-; Low-level network settings, defaults as per pgbouncer:
-;
-;   https://pgbouncer.github.io/config.html#low-level-network-settings
-;
-; tcp_keepcnt, tcp_keepidle, and tcp_keepidle, supported with defaults
-; for Linux as documented above.
-;
-; These defaults match both pgbouncer 1.7 and pgbouncer HEAD.
-;
-;   https://github.com/pgbouncer/pgbouncer/blob/pgbouncer_1_7/etc/pgbouncer.ini
-;   https://github.com/pgbouncer/pgbouncer/blob/master/etc/pgbouncer.ini
-;
-pkt_buf = ${PGBOUNCER_PKT_BUF:-4096}
-max_packet_size = ${PGBOUNCER_MAX_PACKET_SIZE:-2147483647}
-listen_backlog = ${PGBOUNCER_LISTEN_BACKLOG:-128}
-sbuf_loopcnt = ${PGBOUNCER_SBUF_LOOPCNT:-5}
-suspend_timeout = ${PGBOUNCER_SUSPEND_TIMEOUT:-10}
-tcp_defer_accept = ${PGBOUNCER_TCP_DEFER_ACCEPT:-45}
-tcp_keepalive = ${PGBOUNCER_TCP_KEEPALIVE:-1}
-tcp_keepcnt = ${PGBOUNCER_TCP_KEEPCNT:-9}
-tcp_keepidle = ${PGBOUNCER_TCP_KEEPIDLE:-7200}
-tcp_keepintvl = ${PGBOUNCER_TCP_KEEPINTVL:-75}
-EOFEOF
-
 # If PGBOUNCER_STATS_USERNAME and PGBOUNCER_STATS_PASSWORD are
 # defined, enable SHOW commands from pgbouncer with those credentials.
 #
