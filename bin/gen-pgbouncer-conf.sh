3e459cdcb1c1e44d25b5e75c84c04883881db046
jhwillett <jhw@prosperworks.com>
2019-01-09 17:53:06 -0800

    Support pgbouncer config server_connect_timeout per env PGBOUNCER_SERVER_CONNECT_TIMEOUT default 15 as per https://pgbouncer.github.io/config.html.

--- bin/gen-pgbouncer-conf.sh
+++ bin/gen-pgbouncer-conf.sh
@@ -35,6 +35,7 @@ reserve_pool_size = ${PGBOUNCER_RESERVE_POOL_SIZE:-1}
 reserve_pool_timeout = ${PGBOUNCER_RESERVE_POOL_TIMEOUT:-5.0}
 server_lifetime = ${PGBOUNCER_SERVER_LIFETIME:-3600}
 server_idle_timeout = ${PGBOUNCER_SERVER_IDLE_TIMEOUT:-600}
+server_connect_timeout = ${PGBOUNCER_SERVER_CONNECT_TIMEOUT:-15}
 log_connections = ${PGBOUNCER_LOG_CONNECTIONS:-1}
 log_disconnections = ${PGBOUNCER_LOG_DISCONNECTIONS:-1}
 log_pooler_errors = ${PGBOUNCER_LOG_POOLER_ERRORS:-1}
